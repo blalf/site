1ce99586997d007837107f99768e2c5fa93d6375
blalf LeBest <gueydon.sacha@gmail.com>
2021-05-09 03:41:10 +0200

    avc test+php7

---
+++ action_page.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Test Formulaire PHP</title>
+	</head>
+
+
+	<body>
+		<h1>Bonjour,  <?php echo $_GET['nom'] ?></h1>
+		
+
+
+	</body>
+
+</html>
